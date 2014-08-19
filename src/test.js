592f3de60a335abe84e137ce2939412ea7a7307e
David Smith <bigdave.smith@gmail.com>
2014-08-19 13:30:38 -0700

    Problem 9 - Special Pythagorean triplet

--- src/test.js
+++ src/test.js
@@ -288,4 +288,9 @@ describe('euler.js', function() {
       assert.equal(23514624000, Euler.problem8(example, 13));
     });
   });
+  describe('Problem 9 - Special Pythagorean triplet', function() {
+    it('should find the product abc of the triplet a+b+c=1000', function() {
+      assert.equal(31875000, Euler.problem9(1000));
+    });
+  });
 });
