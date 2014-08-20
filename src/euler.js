50152f68745fb43beff7bcc2e3c58de1f5ed9039
David Smith <bigdave.smith@gmail.com>
2014-08-19 21:13:29 -0700

    Optimization of problem 10

--- src/euler.js
+++ src/euler.js
@@ -168,15 +168,20 @@ module.exports.problem9 = function(sum) {
 }
 
 /*
- * Problem 10 -
+ * Problem 10 - Summation of primes
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  *
  * Find the sum of all the primes below two million.
  */
 module.exports.problem10 = function(limit) {
-  var sum = 2;
+  var sum = 10;
 
-  for (var i = 3; i < limit; i += 2) {
+  for (var i = 7; i < limit; i += 2) {
+    // Skip checking isPrime for multiples of 3 or 5, seive style
+    if (i % 3 === 0 || i % 5 === 0) {
+      continue;
+    }
+
     if (Util.isPrime(i)) {
       sum += i;
     }
