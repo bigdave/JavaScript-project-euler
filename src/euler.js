910645387ee0f6c05d1d27d4ba076f7cd1df467a
David Smith <bigdave.smith@gmail.com>
2014-08-23 19:30:14 -0700

    Problem 13 - Large sum

--- src/euler.js
+++ src/euler.js
@@ -268,3 +268,18 @@ module.exports.problem12 = function(numberOfDivisors) {
 
   return number;
 }
+
+/*
+ * Problem 13 - Large sum
+ *
+ * Work out the first ten digits of the sum of the following one-hundred
+ * 50-digit numbers. (Numbers are in the test cases)
+ */
+module.exports.problem13 = function(numbers) {
+  var sum = 0;
+  numbers.forEach(function(element, index, array) {
+    sum += element;
+  });
+
+  return sum.toString().substr(0,10);
+}
