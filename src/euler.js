856216ae8ff9a215d76348f9acce7bd17aa2ef54
David Smith <bigdave.smith@gmail.com>
2014-08-23 17:15:59 -0700

    Problem 11 - Largest product in a grid

--- src/euler.js
+++ src/euler.js
@@ -196,5 +196,52 @@ module.exports.problem10 = function(limit) {
  * direction (up, down, left, right, or diagonally) in the 20×20 grid?
  */
 module.exports.problem11 = function(dataset, length) {
-  
+  if (dataset.length < length) {
+    throw "Dataset is too small for specified chain length";
+  }
+
+  var maxProduct = 0;
+  function replaceMaxIfGreater(x) {
+    if (x > maxProduct) {
+      maxProduct = x;
+    }
+  }
+
+  for (var y = 0; y < dataset.length - length; y++) {
+    for (var x = 0; x < dataset[y].length - length; x++) {
+      // Starting point is x,y
+
+      // Test right
+      var currentProduct = 1;
+      for (var i = 0; i < length; i++) {
+        currentProduct *= dataset[y][x+i];
+      }
+      replaceMaxIfGreater(currentProduct);
+
+      // Test down
+      currentProduct = 1;
+      for (var i = 0; i < length; i++) {
+        currentProduct *= dataset[y+i][x];
+      }
+      replaceMaxIfGreater(currentProduct);
+
+      // Test diagonal right/down
+      currentProduct = 1;
+      for (var i = 0; i < length; i++) {
+        currentProduct *= dataset[y+i][x+i];
+      }
+      replaceMaxIfGreater(currentProduct);
+
+      // Test diagonal right/up
+      if (y > length) {
+        currentProduct = 1;
+        for (var i = 0; i < length; i++) {
+          currentProduct *= dataset[y-i][x+i];
+        }
+        replaceMaxIfGreater(currentProduct);
+      }
+    }
+  }
+
+  return maxProduct;
 }
