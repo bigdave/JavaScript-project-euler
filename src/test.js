819a5bfb9156f6af9340b9dadaf8da632a3a9f1a
David Smith <bigdave.smith@gmail.com>
2014-08-23 19:05:16 -0700

    Problem 12 - Highly divisible triangular number

--- src/test.js
+++ src/test.js
@@ -359,4 +359,14 @@ describe('euler.js', function() {
       assert.equal(70600674, Euler.problem11(example, 4));
     });
   });
+  describe('Problem 12 - Highly divisible triangular number', function() {
+    it('should work for the Euler examples', function() {
+      assert.equal(1, Euler.problem12(1));
+      assert.equal(3, Euler.problem12(2));
+      assert.equal(6, Euler.problem12(3));
+      assert.equal(6, Euler.problem12(4));
+      assert.equal(28, Euler.problem12(5));
+      assert.equal(76576500, Euler.problem12(500));
+    });
+  });
 });
